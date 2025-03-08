0923d913ebfa460ac1dd17fa6bd4319b8035da5e
Mahmut Emre Narol <mahmutemrenarol@gmail.com>
2025-03-08 13:33:59 +0300

    Acunmedya Projects

---
+++ Week 2/task.ts
@@ -0,0 +1,13 @@
+//1 ile 50 arasındaki çift sayıları toplayan bir program yazınız
+let toplam:number=0;
+for(let i:number=0;i <= 50;i++){
+if(i %2==0){
+    toplam=toplam+i;
+}
+}
+console.log(toplam);
+
+//7 Sayısının çarpım tablosu
+for(let j:number=1;j <= 10;j++){
+    console.log(j*7)
+}
